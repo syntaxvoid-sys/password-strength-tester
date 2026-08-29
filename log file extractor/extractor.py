841817950bdb590872afdd6e20e9a77bb7449214
syntaxvoid-sys <leonfink333@gmail.com>
2026-08-29 23:52:50 +0200

    Delete log file extractor directory

--- log file extractor/extractor.py
+++
@@ -1,6 +0,0 @@
-with open("server.log.txt" , "r") as log_file:
- with open("error_report.txt" , "w") as report_file:
-    for line in log_file:
-        if "ERROR" in line:
-            report_file.write(line)
-             
