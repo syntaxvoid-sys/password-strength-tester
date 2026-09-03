db0f571890ccef7b16fb88a01d014fffd3343a01
syntaxvoid-sys <leonfink333@gmail.com>
2026-09-04 01:06:09 +0200

    Implement password strength scoring system
    Added scoring system to evaluate password strength based on criteria.

--- password-strength-tester.py
+++ password-strength-tester.py
@@ -4,6 +4,8 @@ numbers = "1234567890"
 has_number = False
 has_symbols = False
 is_8_long = False
+score = 0
+weak = 0
 
 if len(password) >= 8:
     is_8_long = True
@@ -14,24 +16,29 @@ for character in password:
 
     if character in numbers:
         has_number = True 
-        
 
 if has_number:
-      print("has atleast one number")  
+      print("has atleast one number") 
+      score += 1 
 else:
      print("needs atleast one number")
-    
 if has_symbols:
       print("has atleast one symbol")
+      score += 1
 else:
      print("need atleast one symbol")
-
 if is_8_long:
       print("is atleast 8 characters long")
+      score += 1
 else:
      print("password needs to be 8 characters long")
-
-if has_number and has_symbols and is_8_long:
-      print("password is strong")
+if score == 0:
+     print("password is SUPER WEAK")
+elif score == 1:
+     print("passwors is weak")
+elif score == 2:
+     print("password is weak but okay")
 else:
-     print("password is weak")
+     print("Password is strong")
+
+print("score is", score, "out of 3" )
