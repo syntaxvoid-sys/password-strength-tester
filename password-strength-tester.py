d60b5832f880b059de45eb7ac5b507f0f13b708f
syntaxvoid-sys <leonfink333@gmail.com>
2026-09-06 00:10:11 +0200

    Enhance password strength tester with uppercase check
    Added uppercase letter check and updated score calculation.

--- password-strength-tester.py
+++ password-strength-tester.py
@@ -4,8 +4,12 @@ numbers = "1234567890"
 has_number = False
 has_symbols = False
 is_8_long = False
+has_uppercase = False
 score = 0
 weak = 0
+
+if password .upper:
+     has_uppercase = True
 
 if len(password) >= 8:
     is_8_long = True
@@ -32,13 +36,18 @@ if is_8_long:
       score += 1
 else:
      print("password needs to be 8 characters long")
+if has_uppercase:
+     print("has atleast one uppercase")
+     score += 1
 if score == 0:
      print("password is SUPER WEAK")
 elif score == 1:
      print("passwors is weak")
 elif score == 2:
      print("password is weak but okay")
+elif score == 3:
+     print("Password is kinda strong")
 else:
      print("Password is strong")
 
-print("score is", score, "out of 3" )
+print("score is", score, "out of 4" )
